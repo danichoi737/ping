abe1b9e6ac788b5e46738c55e043a129ad198fbb
dainchoi737 <danichoi737@gmail.com>
2023-02-03 23:50:57 +0900

    Add CMakeLists and main code

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+/*
+ *  main
+ */
+
+#include <iostream>
+#include <getopt.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+  char *target {};
+
+  // Parse command line options
+  int opt {};
+  while ((opt = getopt(argc, argv, "h?" "V")) != EOF) {
+    switch (opt) {
+    case 'V':
+      std::cout << "PING v0.1.0" << std::endl;
+      exit(EXIT_SUCCESS);
+      break;
+
+    default:
+      break;
+    }
+  }
+  argc -= optind;
+  argv += optind;
+
+  if (argc == 0) {
+    std::cerr << "Usage error" << std::endl;
+    // TO-DO: print usage
+    exit(2);
+  }
+
+  // Save the target IP address from argv
+  target = argv[argc - 1];
+}
